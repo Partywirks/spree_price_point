a98142944dacd791e43c6fec909165e7c49dddcf
edward sharp <edward@edwardsharp.net>
2015-12-05 01:08:56 -0800

    yank unnecessary override

--- spree_price_point/app/models/spree/order_decorator.rb
+++ spree_price_point/app/models/spree/order_decorator.rb
@@ -1,12 +1,5 @@
 Spree::Order.class_eval do
 
-	def find_line_item_by_variant(variant, options = {})
-    line_items.detect { |line_item|
-                  line_item.variant_id == variant.id &&
-                  line_item_options_match(line_item, options)
-                }
-  end
-
   def line_item_options_match(line_item, options)
       return true unless options
 
